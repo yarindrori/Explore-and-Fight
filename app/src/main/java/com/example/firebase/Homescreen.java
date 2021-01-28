805fb587c257beb5ed5aac0ffdf3a7e7f2808b16
yarindrori <77576685+yarindrori@users.noreply.github.com>
2021-01-28 18:40:33 +0200

    28/01 - score , coins , and game finished!

--- app/src/main/java/com/example/firebase/Homescreen.java
+++ app/src/main/java/com/example/firebase/Homescreen.java
@@ -39,20 +39,25 @@ public class Homescreen extends AppCompatActivity {
         ld = findViewById(R.id.ldimg);
         String id = user.getUid();
         Intent intent = getIntent();
-        String k = intent.getStringExtra("key");
-        if (k.equals("yes"))
-        {
-            try {
-                DatabaseReference r = FirebaseDatabase.getInstance().getReference("Games");
-                r.removeValue();
-                DatabaseReference r1 = FirebaseDatabase.getInstance().getReference("Score");
-                r1.removeValue();
-            }
-            catch (Exception e)
+        try {
+            String k = intent.getStringExtra("key");
+            if (k.equals("yes"))
             {
-
+                try {
+                    DatabaseReference r = FirebaseDatabase.getInstance().getReference("Games");
+                    r.removeValue();
+                    DatabaseReference r1 = FirebaseDatabase.getInstance().getReference("Score");
+                    r1.removeValue();
+                }
+                catch (Exception e)
+                {
+                }
             }
         }
+        catch (Exception e)
+        {
+        }
+
         ref = FirebaseDatabase.getInstance().getReference().child("Users").child(id);
         ref.addValueEventListener(new ValueEventListener() {
             @Override
