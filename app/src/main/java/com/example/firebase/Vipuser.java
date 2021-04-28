d928ebe9d7863d33f0dc753ba728e23797d7698e
yarindrori <77576685+yarindrori@users.noreply.github.com>
2021-04-28 14:35:30 +0300

    28 /04 - ALL DONE , - HOVERT

--- app/src/main/java/com/example/firebase/Vipuser.java
+++ app/src/main/java/com/example/firebase/Vipuser.java
@@ -8,44 +8,12 @@ public class Vipuser extends Users{
         super(mail,password,coins,points,username);
         this.Access = Access;
     }
-
-    @Override
-    public void setUsername(String username) {
-        super.setUsername(username);
+    public Boolean getAccess() {
+        return Access;
     }
-    @Override
-    public String getMail() {
-        return super.getMail();
+    public void setAccess(Boolean access) {
+        Access = access;
     }
-    @Override
-    public void setMail(String mail) {
-        super.setMail(mail);
-    }
-    @Override
-    public String getPassword() {
-        return super.getPassword();
-    }
-    @Override
-    public void setPassword(String password) {
-        super.setPassword(password);
-    }
-    @Override
-    public int getCoins() {
-        return super.getCoins();
-    }
-    @Override
-    public void setCoins(int coins) {
-        super.setCoins(coins);
-    }
-    @Override
-    public int getPoints() {
-        return super.getPoints();
-    }
-    @Override
-    public void setPoints(int points) {
-        super.setPoints(points);
-    }
-
     @Override
     public String toString() {
         return super.toString();
